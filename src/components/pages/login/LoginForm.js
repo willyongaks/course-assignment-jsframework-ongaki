d66ee5c96a975c78f29af5c29cdfec2225eab35c
willyongaks <william.ongaki10@gmail.com>
2022-12-29 22:14:45 +0100

    n

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import FormError from "../../common/FormError";
 
 const schema = yup.object().shape({
     username: yup.string().required("please enter your username"),
@@ -12,20 +13,25 @@ const schema = yup.object().shape({
 function LoginForm() {
     const [submitting, setSubmitting] = useState(false);
     const [loginError, setLoginError] = useState(null);
+
+
     const { register, handleSubmit, formState: {errors}, } = useForm({
         resolver: yupResolver(schema)
     })
   return (
     <>
         <form onSubmit={handleSubmit()}>
+            {loginError && <FormError>{loginError}</FormError>}
             <fieldset disabled={submitting}>
                 <div>
                     <label>Username</label><br/>
                     <input name="username" placeholder="username" {...register} />
+                    {errors.username && <FormError>{errors.username.message}</FormError>}
                 </div>
                 <div>
                     <label>password</label><br/>
                     <input name="password" placeholder="username" {...register} type="password"/>
+                    {errors.password && <FormError>{errors.password.message}</FormError>}
                 </div>
 
                 <button>{submitting ? "Loggin in..." : "Login"}</button>
