4e748ddb6b277c6374ea395302f56cba682013fa
willyongaks <william.ongaki10@gmail.com>
2023-01-07 14:23:42 +0100

    n

--- src/components/pages/favourites/FavsButton.js
+++ src/components/pages/favourites/FavsButton.js
@@ -1,18 +1,23 @@
-
-import { FaHeart } from 'react-icons/fa';
+import "./index.css"
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+import { useState } from "react";
 
 
 const FavouriteButton = ({ game}) => {
+    const [isFavourite, setIsFavourite] = useState(false);
+
+
     const handleFavsClick = (event) => {
         event.preventDefault();
+        setIsFavourite(!isFavourite);
 
         localStorage.setItem("gameId", game.id);
         localStorage.setItem("gameTitle", game.title);
     }
 
     return (
-        <button onClick={handleFavsClick}>
-        <FaHeart />
+        <button onClick={handleFavsClick} className="favs-button">
+        {isFavourite ? < FaHeart /> : <FaRegHeart />}
         </button>
     );
 }
